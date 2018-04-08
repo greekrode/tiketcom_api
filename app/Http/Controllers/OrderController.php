bf4d90e6fe175fa2dc41141026facbc8da621060
albert wijaya <albertsagi97@gmail.com>
2018-04-08 23:45:13 +0700

    move model path

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
-
+use App\Order;
+use App\OrderDetail;
+use App\Passenger;
+use Auth;
 
 class OrderController extends Controller
 {
